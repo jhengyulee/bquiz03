1d6e446037ad01d939b5795e106bd791d16187c1
jhengyulee <chi1987615@hotmail.com>
2022-08-08 16:33:40 +0800

    更新資料庫

--- db24_3.sql
+++ db24_3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2022-08-08 02:19:44
+-- 產生時間： 2022-08-08 10:32:44
 -- 伺服器版本： 10.4.24-MariaDB
 -- PHP 版本： 8.1.6
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `db24_3`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `movie`
+--
+
+CREATE TABLE `movie` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `length` int(11) UNSIGNED NOT NULL,
+  `ondate` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `publish` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `director` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `trailer` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `poster` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `intro` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rank` int(11) UNSIGNED NOT NULL,
+  `sh` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `movie`
+--
+
+INSERT INTO `movie` (`id`, `name`, `level`, `length`, `ondate`, `publish`, `director`, `trailer`, `poster`, `intro`, `rank`, `sh`) VALUES
+(4, '888', '輔導級', 888, '2022-5-5', '888', '888', '03B01v.mp4', '03B01.png', '888', 1, 1),
+(5, '泰山職訓生死鬥', '限制級', 965, '2022-9-15', '泰山職訓場', '泰山職訓場', '03B04v.mp4', '03B07.png', '泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥泰山職訓生死鬥', 5, 1),
+(9, '預告片3', '保護級', 30, '2022-9-14', '宜蘭職訓場', '宜蘭訓練場', '03B25v.mp4', '03B25.png', 'ㄎㄎㄎㄎㄎㄎㄎㄎ', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -41,14 +71,25 @@ CREATE TABLE `poster` (
 --
 
 INSERT INTO `poster` (`id`, `img`, `name`, `rank`, `sh`, `ani`) VALUES
-(1, '03A01.jpg', '預告片1', 1, 1, 3),
-(2, '03A02.jpg', '預告片2', 2, 1, 2),
-(3, '03A03.jpg', '預告片3', 3, 1, 3),
-(4, '03A04.jpg', '預告片4', 4, 1, 3);
+(1, '03A01.jpg', '蠟筆小新', 2, 1, 3),
+(2, '03A02.jpg', '預告片2', 3, 1, 2),
+(3, '03A03.jpg', '預告片3', 1, 1, 3),
+(4, '03A04.jpg', '預告片4', 4, 1, 3),
+(5, '03A05.jpg', '預告片5', 5, 1, 2),
+(6, '03A06.jpg', '預告片6', 6, 1, 2),
+(7, '03A07.jpg', '預告片7', 7, 1, 1),
+(8, '03A08.jpg', '預告片8', 8, 1, 3),
+(9, '03A09.jpg', '預告片9', 9, 1, 1);
 
 --
 -- 已傾印資料表的索引
 --
+
+--
+-- 資料表索引 `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `poster`
@@ -61,10 +102,16 @@ ALTER TABLE `poster`
 --
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `poster`
 --
 ALTER TABLE `poster`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
